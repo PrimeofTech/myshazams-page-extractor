6d52843961c3c40351e3fa6a2ecc069596be4b35
PrimeofTech <primeoftech0@gmail.com>
2019-12-13 17:49:02 +0400

    v-0.0.1

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from bs4 import BeautifulSoup as bs
 
-print('='*95)
-
 with open('shazam-html-files/shazam.html', 'r', encoding='UTF-8') as file:
     content = file.read()
 
 soup = bs(content, 'html.parser')
-
-# for item in soup.find_all('li'):
-#     if len(list(item.text)) > 10:
-#         print(item, '<br>')
 
 def list_cleaner(lst):
     new_lst = []
