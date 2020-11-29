3019bf0f9eb7bc8604d4d938d7ff47f022454973
A. S <primeoftech0@gmail.com>
2020-11-29 12:27:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as bs
 
-with open('shazam-html-files/shazam.html', 'r', encoding='UTF-8') as file:
+with open('My Shazam - Shazam.html', 'r', encoding='UTF-8') as file:
     content = file.read()
 
 soup = bs(content, 'html.parser')
@@ -13,13 +13,17 @@ def list_cleaner(lst):
             new_lst.append(i)
     return new_lst
 
-with open('shazam-html-files/shazam.csv', 'a+') as f:
+with open('shazam.csv', 'a+') as f:
 
     print('no, name, artist')
     f.write('no, name, artist \n')
-    for item in soup.find_all('article', {'data-screenname': 'myshazams '}):
-        tag = bs(item.text, 'html.parser')
-        li = tag.text.split('   ')
-        li = list_cleaner(li)
-        print(f'{li[0]}, {li[1]}, {li[2]}')
-        f.write(f'{li[0]}, {li[1]}, {li[2]} \n')
+    for item in soup.find_all('article', {'data-screenname': 'charts'}):
+        no = ' '.join(item.find_all('div', class_='number')[0].text.split())
+        title = ' '.join(item.find_all('div', class_='title')[0].text.split())
+        artist = ' '.join(item.find_all('div', class_='artist')[0].text.split())
+        if ',' in title:
+            title = f'"{title}"'
+        if ',' in artist:
+            artist = f'"{artist}"'
+        print(f'{no}, {title}, {artist}')
+        f.write(f'{no}, {title}, {artist} \n')
